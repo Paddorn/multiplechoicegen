4ae8cd7e09c2e5be659a565bd0e1d6dfca2bb28e
James B <Jamesbarlow90@gmail.com>
2022-09-02 19:55:19 -0400

    iteration changes

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+function submitQuestionAmount()
+{
+    const choice = ["A", "B", "C", "D"];
+    const numQuestions = document.querySelector('#numQuestions').value;
+    const answerList= [];
+
+    for (let i=0; i<numQuestions; i++){
+        answerList.push(i);
+        for(let i=0; i<answerList.length-1; i++){
+            
+        }
+
+    }
+
+}
